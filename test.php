533166a6024d54d6d4fb208e19c216270c7c2022
maharjannivaa <maharjan.niva@webexpertsnepal.com>
2023-01-31 13:03:40 +0545

    Revert "some changes"
    This reverts commit aee5c423d600b503332ba5b5217083432572d319.

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
   <title>PHP-Test</title>
  </head>
  <body>
-  <?php echo '<h1>Lorem ipsum is simply a dummy text &%*&%*&%*%(*^(*^(</h1>'
+  <?php echo '<h1>Lorem ipsum is simply a dummy text</h1>'
   ?>
  </body>
 </html>
