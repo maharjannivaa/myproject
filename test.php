f3817e2a5d81bd786e8ed3ccdf9fe6ec53397236
maharjannivaa <maharjan.niva@webexpertsnepal.com>
2023-01-31 10:57:12 +0545

    last commit

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
   <title>PHP-Test</title>
  </head>
  <body>
-  <?php echo '<h1>Hello php world...............this is some changes u dnt want to commit</h1>
-  <h3>Welcome to php ?>
+  <?php echo '<h1>Last commit</h1>'
+  ?>
  </body>
 </html>
